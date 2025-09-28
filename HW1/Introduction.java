e571419088bceedb141ab780313e72909aaac245
windsong <voanhtai4006@gmail.com>
2025-09-28 23:01:09 +0700

    add Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Võ Anh Tài \t 24020303 \t INT2204 6 \t w1ndS0ng \t voanhtai4006@gmail");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.");
+        System.out.println("Take one down, pass it around, ");
+        System.out.println("8 bottles of beer on the wall, 8 bottles of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("...");
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
